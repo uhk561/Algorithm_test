8f30ab52d349b83506038592ef735a625b900ca5
uhk561 <uhk561@naver.com>
2025-10-22 09:23:31 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 88 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double solution(int[] numbers) {
+        double answer = 0;
+
+      for(int number : numbers) {
+          answer += number;
+      }
+        answer = answer / numbers.length;
+        return answer;
+    }
+} 
+
+        
