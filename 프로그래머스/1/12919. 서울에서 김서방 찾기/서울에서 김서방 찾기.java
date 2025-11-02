abfd91cce04e7528cae79c582bf5f2455870bba9
uhk561 <uhk561@naver.com>
2025-11-02 14:57:14 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 7.68 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String[] seoul) {
+        int a = 0;
+        for(int i = 0; i < seoul.length; i++) {
+            if (seoul[i].contains("Kim") == true) {
+                a = i;
+                 break;
+            } 
+        }
+        String answer = "김서방은 " + a + "에 있다";
+        return answer;
+    }
+}
