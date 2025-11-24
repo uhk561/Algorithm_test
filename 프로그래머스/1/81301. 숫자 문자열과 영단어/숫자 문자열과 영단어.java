f40656781047d8af008b6a720225e83d30ba5a3b
uhk561 <uhk561@naver.com>
2025-11-24 21:43:14 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.13 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String[][] num = {{"zero", "0"}, {"one", "1"}, {"two", "2"}, {"three", "3"}, {"four", "4"}, {"five", "5"}, {"six", "6"},
+                          {"seven", "7"}, {"eight", "8"},  {"nine", "9"}};
+        for (int i = 0; i < num.length; i++) {
+            if(s.contains(num[i][0])) {
+               s = s.replace(num[i][0], num[i][1]);
+            }
+        }
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+}
