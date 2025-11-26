ba4af052571937653a812219e3706da716ffb934
uhk561 <uhk561@naver.com>
2025-11-26 17:15:15 +0900

    [level 1] Title: K번째수, Time: 0.45 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for(int i = 0; i < commands.length; i++){
+            int start = commands[i][0] - 1;
+            int end = commands[i][1];
+            int num = commands[i][2] - 1;
+            
+            int[] temp = Arrays.copyOfRange(array, start, end);
+            Arrays.sort(temp);
+
+            answer[i] = temp[num];
+        }
+
+        return answer;
+    }
+}
