d024e6fed7099144a4cc1b720c19da4b4749ce0a
정순관 <uhk561@naver.com>
2025-12-26 20:39:24 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 1.30 ms, Memory: 88.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for (int i = my_string.length(); i >= 1; i--) {
+           answer +=  my_string.substring(i - 1, i);
+        }
+        return answer;
+    }
+}
