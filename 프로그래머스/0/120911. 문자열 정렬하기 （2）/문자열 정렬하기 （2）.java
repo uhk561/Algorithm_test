3d619032bdc2484ace5e840e63a6b39860e83acd
정순관 <uhk561@naver.com>
2026-03-31 16:38:52 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.50 ms, Memory: 89.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String my_string) {
+        my_string = my_string.toLowerCase();  
+        
+        char[] arr = my_string.toCharArray();
+        Arrays.sort(arr);                    
+        
+        return new String(arr);
+    }
+}
