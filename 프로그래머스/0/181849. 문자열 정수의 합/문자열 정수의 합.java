1b9cded71e8f051ef9e129c6cbc81f66b64b8f55
정순관 <uhk561@naver.com>
2026-04-20 16:56:46 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.05 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String num_str) {
+        int sum = 0;
+        
+        for (char c : num_str.toCharArray()) {
+            sum += c - '0';
+        }
+        
+        return sum;
+    }
+}
