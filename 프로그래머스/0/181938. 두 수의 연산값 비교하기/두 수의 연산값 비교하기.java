8fa7b61ffc5c0bf0c917f27651af92e9c0198508
정순관 <uhk561@naver.com>
2026-05-01 20:31:16 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 1.75 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int a, int b) {
+        int ab = Integer.parseInt(String.valueOf(a) + String.valueOf(b));
+        int mul = 2 * a * b;
+        
+        return Math.max(ab, mul);
+    }
+}
