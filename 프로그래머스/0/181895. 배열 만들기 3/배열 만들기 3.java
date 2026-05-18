cbd7401dae76849910ee00e669913b0e2e7a1199
정순관 <uhk561@naver.com>
2026-05-18 21:17:41 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.94 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        
+        int a1 = intervals[0][0];
+        int b1 = intervals[0][1];
+        
+        int a2 = intervals[1][0];
+        int b2 = intervals[1][1];
+        
+        int size1 = b1 - a1 + 1;
+        int size2 = b2 - a2 + 1;
+        
+        int[] answer = new int[size1 + size2];
+        
+        int idx = 0;
+        for(int i = a1; i <= b1; i++) {
+            answer[idx] = arr[i];
+            idx++;
+        }
+        
+        for(int i = a2; i <= b2; i++) {
+            answer[idx] = arr[i];
+            idx++;
+        }
+        
+        return answer;
+    }
+}
