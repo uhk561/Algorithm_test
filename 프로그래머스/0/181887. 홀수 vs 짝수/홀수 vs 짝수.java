0facf8ead4ec07026d726ce9fe3a2d4a9f289b09
정순관 <uhk561@naver.com>
2026-06-15 21:05:28 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int[] num_list) {
+        
+        int odd = 0;
+        int even = 0;
+        
+        for(int i = 0; i < num_list.length; i++) {
+            
+            if((i + 1) % 2 == 1) {
+                odd += num_list[i];
+            } 
+            else {
+                even += num_list[i];
+            }
+        }
+        
+        if(odd > even) {
+            return odd;
+        } else {
+            return even;
+        }
+    }
+}
