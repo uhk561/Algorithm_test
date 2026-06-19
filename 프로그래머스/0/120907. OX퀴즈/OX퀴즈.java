cc13801497a359fb780e8104a3fe6aa324fe7d3a
정순관 <uhk561@naver.com>
2026-06-19 22:06:15 +0900

    [level 0] Title: OX퀴즈, Time: 0.12 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String[] solution(String[] quiz) {
+        
+        String[] answer = new String[quiz.length];
+        
+        for(int i = 0; i < quiz.length; i++) {
+            
+            String[] arr = quiz[i].split(" ");
+            
+            int x = Integer.parseInt(arr[0]);
+            String op = arr[1];
+            int y = Integer.parseInt(arr[2]);
+            int z = Integer.parseInt(arr[4]);
+            
+            int result = 0;
+            
+            if(op.equals("+")) {
+                result = x + y;
+            } else {
+                result = x - y;
+            }
+            
+            if(result == z) {
+                answer[i] = "O";
+            } else {
+                answer[i] = "X";
+            }
+        }
+        
+        return answer;
+    }
+}
