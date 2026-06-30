1c55123686dc3d3d554f9a982ec857946cf81a51
정순관 <uhk561@naver.com>
2026-07-01 00:12:55 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 6.36 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -4,7 +4,6 @@ class Solution {
     public int solution(int[] numbers) {
         Arrays.sort(numbers);
         int n = numbers.length;
-
         int a = numbers[0] * numbers[1];        
         int b = numbers[n - 1] * numbers[n - 2]; 
 
