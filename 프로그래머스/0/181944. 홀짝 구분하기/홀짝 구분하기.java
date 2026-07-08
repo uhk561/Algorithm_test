81da475c3c3edeb8dc2b36996889756723340ebe
정순관 <uhk561@naver.com>
2026-07-08 21:03:50 +0900

    [level 0] Title: 홀짝 구분하기, Time: 96.86 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         
