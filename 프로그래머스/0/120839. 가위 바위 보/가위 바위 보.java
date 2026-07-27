9b54479d84575097c5f5d92b3e6299b409578e71
정순관 <uhk561@naver.com>
2026-07-28 01:05:46 +0900

    [level 0] Title: 가위 바위 보, Time: 2.74 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,6 +1,7 @@
 class Solution {
     public String solution(String rsp) {
         String answer = "";
+        
         for(int i = 0; i < rsp.length(); i++) {
             char a = rsp.charAt(i);
             switch (a) {
