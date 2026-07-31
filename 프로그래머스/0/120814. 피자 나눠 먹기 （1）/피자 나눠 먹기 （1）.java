3fe89fd6c1fddf6fb2ca8cacab31266b94079763
정순관 <uhk561@naver.com>
2026-07-31 16:51:02 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
+        
         for(int i = 1; i <= 100; i++) {
             if(n <= (7 * i)) {
                 answer = i;
